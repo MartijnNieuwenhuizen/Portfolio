c17ec97c04ebcaabebfbf7bf84c685107c3b67ad
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-07-11 12:24:57 +0200

    added the index tot the title and removed an unnecessary window.onload

--- src/js/modules/current-title.js
+++ src/js/modules/current-title.js
@@ -1,35 +1,44 @@
 var currentTitle = {
 	set: function() {
 
-		window.onload = function() {
+		var HTMLelement = document.querySelector('.menu h1');
+		var windowPathname = window.location.pathname;
+		var cutPathname = windowPathname.slice(1);
 
-			var HTMLelement = document.querySelector('.menu h1');
-			var windowPathname = window.location.pathname;
+		// If the url contains only one '/'
+		if ( windowPathname.slice(1).indexOf("/") == -1 ) {
 
-			if ( windowPathname.slice(1).indexOf("/") == -1 ) {
-				
-				var cutPathname = windowPathname.slice(1);
-				var content = cutPathname.slice(0, cutPathname.indexOf(".html"));
-
-				if ( content.indexOf("-") ) {
-					var content = content.replace("-", " ");
-				}
-
-				HTMLelement.innerHTML = content;
-
-				return;
-			}
-
-			var cutPathname = windowPathname.slice(1);
-			var content = cutPathname.slice(0, cutPathname.indexOf("/"));
-
-			if ( content.indexOf("-") ) {
-				var content = content.replace("-", " ");
-			}
-
+			var content = cutPathname.slice(0, cutPathname.indexOf(".html"));
+			content = currentTitle.replaceIndex(content);
+			content = currentTitle.replaceDash(content);
 			HTMLelement.innerHTML = content;
+			return;
+		}
 
-		};
+		// If the url contains only one '/'
+		var content = cutPathname.slice(0, cutPathname.indexOf("/"));
+		content = currentTitle.replaceDash(content);
+		HTMLelement.innerHTML = content;
+
+	},
+
+	// Check if there's a '-' in the name and replace if this is true
+	replaceDash: function(content) {
+
+		if ( content.indexOf("-") ) {
+			content = content.replace("-", " ");
+		}
+		return content;
+
+	},
+
+	// If the content is index or nothing, replace content with "Hello"
+	replaceIndex: function(content) {
+
+		if ( content == "index" || content == "" ) {
+			content = "Hello"
+		}
+		return content;
 
 	}
 }
